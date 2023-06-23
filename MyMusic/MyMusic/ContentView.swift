ff83fa80f8ac14161ba4a59d34fd1e73840189a9
Sho <119092087+ShoNakamura5@users.noreply.github.com>
2023-06-23 15:24:33 +0900

    Add files via upload

---
+++ MyMusic/MyMusic/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  MyMusic
+//
+//  Created by 中村翔 on 2023/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // 音を鳴らすためのSoundPlayerクラスのインスタンス生成
+    let soundPlayer = SoundPlayer()
+    
+    var body: some View {
+        ZStack {
+            // 背景画像を指定する
+            Image("background")
+            // リサイズする
+                .resizable()
+            // 画面いっぱいになるようにセーフエリア外まで表示されるように指定
+                .ignoresSafeArea()
+            // アスペクト比（縦横比）を維持して短辺に収まるようにする
+                .scaledToFill()
+            
+            // 水平にレイアウト（横方向にレイアウト）
+            HStack {
+                // シンバルボタン
+                Button {
+                    // ボタンをタップした時のアクション
+                    // シンバルの音を鳴らす
+                    soundPlayer.cymbalPlay()
+                } label: {
+                    // 画像を表示する
+                    Image("cymbal")
+                } // シンバルはここまで
+                
+                // ギターボタン
+                Button {
+                    // ボタンを押した時のアクション
+                    // ギターの音を鳴らす
+                    soundPlayer.guitarPlay()
+                } label: {
+                    // 画像を表示する
+                    Image("guitar")
+                } // ギターボタンはここまで
+            } // HStack ここまで
+        } // ZStack ここまで
+    } // body ここまで
+} // ContentView ここまで
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 中村翔 on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
