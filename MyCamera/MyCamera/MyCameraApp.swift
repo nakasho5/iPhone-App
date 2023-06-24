e943c57b35994fea12d3878160d0a16a36eb3787
Sho <119092087+ShoNakamura5@users.noreply.github.com>
2023-06-24 18:21:59 +0900

    Add files via upload

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 中村翔 on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
